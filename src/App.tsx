7a913acaa2b0e54d31aec08ade5829ce856039cf
Lawrence E. Bee <124285505+Tech-Rocket@users.noreply.github.com>
2025-09-08 08:16:23 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/ui/header/header";
+import HeroSection from "./components/hero-section/hero-section";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+    </>
+  );
+}
---
+++ src/components/ui/header/header.tsx
@@ -0,0 +1,25 @@
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import AvatarImg from "/images/image-avatar.png";
+
+export default function Header() {
+  return (
+    <header className="p-5 flex items-center justify-between border-none">
+      <div className="flex items-center  gap-x-3">
+        <GiHamburgerMenu className=" text-gray-600 text-2xl leading-0" />
+        <h3 className="text-slate-800 text-3xl font-bold leading-0 m-0">
+          sneakers
+        </h3>
+      </div>
+
+      <div className="flex items-center gap-x-3">
+        <MdOutlineShoppingCart className="text-gray-600 text-xl" />
+        <img
+          src={AvatarImg}
+          className="h-6 w-6 rounded-full"
+          alt="avatar image"
+        />
+      </div>
+    </header>
+  );
+}
